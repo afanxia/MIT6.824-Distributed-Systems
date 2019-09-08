57a000643f261c00850445f915ff6cf6b962940e
Haitao Liao <liaohaitao142934@163.com>
2019-09-08 22:24:01 +0800

    Lab1 - Part IV: Handling worker failures.

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -34,37 +34,77 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	// Your code here (Part III, Part IV).
 	//
 
-	var wg sync.WaitGroup
+	var (
+		q        []int
+		mu       *sync.Mutex
+		complete bool
+	)
+	// init task queue
 	for i := 0; i < ntasks; i++ {
-		select {
-		case worker := <-registerChan:
-			// Increment the WaitGroup counter.
-			wg.Add(1)
-			// Launch a goroutine to do map/reduce.
-			go func(i int) {
-				args := &DoTaskArgs{
-					JobName:       jobName,
-					File:          mapFiles[i],
-					Phase:         phase,
-					TaskNumber:    i,
-					NumOtherPhase: n_other,
-				}
-
-				// Do real tasks
-				ok := call(worker, "Worker.DoTask", args, nil)
-				if ok == false {
-					fmt.Printf("Worker: RPC %s DoTask error\n", worker)
-				} else {
-					// Decrement the counter when the goroutine completes.
-					wg.Done()
-					// release the worker for next job
-					registerChan <- worker
-				}
-			}(i)
-		}
+		q = append(q, 0)
 	}
-	// Wait for all workers to complete.
-	wg.Wait()
+	mu = &sync.Mutex{}
+
+	for {
+		// var wg sync.WaitGroup
+		wg := &sync.WaitGroup{}
+
+		// if complete, all tasks are done successfully.
+		complete = true
+		mu.Lock()
+		for i := 0; i < ntasks; i++ {
+			if q[i] == 0 {
+				complete = false
+				break
+			}
+		}
+		mu.Unlock()
+		if complete {
+			break
+		}
+
+		// schedule
+		for i := 0; i < ntasks; i++ {
+			mu.Lock()
+			if q[i] == 1 {
+				mu.Unlock()
+				continue
+			}
+			mu.Unlock()
+
+			select {
+			case worker := <-registerChan:
+				wg.Add(1)
+				// Launch a goroutine to do map/reduce.
+				go func(wg *sync.WaitGroup, i int) {
+					args := &DoTaskArgs{
+						JobName:       jobName,
+						File:          mapFiles[i],
+						Phase:         phase,
+						TaskNumber:    i,
+						NumOtherPhase: n_other,
+					}
+
+					// Do real tasks
+					ok := call(worker, "Worker.DoTask", args, nil)
+					if ok == false {
+						fmt.Printf("Worker: RPC %s DoTask %d error, will re-assign it later\n", worker, i)
+					} else {
+						mu.Lock()
+						q[i] = 1
+						mu.Unlock()
+					}
+					// After current worker is done, no matter failed or not, will do following 2 steps.
+					// 1. Decrement the counter when the goroutine completes.
+					wg.Done()
+					// 2. Release the worker for next job
+					registerChan <- worker
+				}(wg, i)
+			}
+		}
+		// Wait for all workers to complete.
+		wg.Wait()
+	}
 
 	fmt.Printf("Schedule: %v done\n", phase)
 }
